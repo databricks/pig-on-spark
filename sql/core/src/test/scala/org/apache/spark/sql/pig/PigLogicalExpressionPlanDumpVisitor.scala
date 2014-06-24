67f1abbd866e9f2bf9589a303911e3bc432d3c66
Gregory Owen <greowen@gmail.com>
2014-06-24 16:08:24 -0700

    Added debugging printers

--- sql/core/src/test/scala/org/apache/spark/sql/pig/PigLogicalExpressionPlanDumpVisitor.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/pig/PigLogicalExpressionPlanDumpVisitor.scala
@@ -1,8 +1,87 @@
 package org.apache.spark.sql.pig
 
-/**
- * Created by Greg on 6/19/14.
- */
-class PigLogicalExpressionPlanDumpVisitor {
+import org.apache.pig.newplan.DependencyOrderWalker
+import org.apache.pig.newplan.logical.expression._
 
+class PigLogicalExpressionPlanDumpVisitor(plan: LogicalExpressionPlan, indent: String = "")
+  extends LogicalExpressionVisitor(plan, new DependencyOrderWalker(plan, true)) {
+
+  def printWithIndent(str: String) = {
+    println(indent + str)
+  }
+
+  def dumpExp(exp: LogicalExpression) = {
+    printWithIndent(exp.toString)
+  }
+
+  override def visit(op: AndExpression) = { dumpExp(op) }
+
+
+  override def visit(op: OrExpression) = { dumpExp(op) }
+
+
+  override def visit(op: EqualExpression) = { dumpExp(op) }
+
+
+  override def visit(op: ProjectExpression) = { dumpExp(op) }
+
+
+  override def visit(op: ConstantExpression) = { dumpExp(op) }
+
+
+  override def visit(op: CastExpression) = { dumpExp(op) }
+
+
+  override def visit(op: GreaterThanExpression) = { dumpExp(op) }
+
+
+  override def visit(op: GreaterThanEqualExpression) = { dumpExp(op) }
+
+
+  override def visit(op: LessThanExpression) = { dumpExp(op) }
+
+
+  override def visit(op: LessThanEqualExpression) = { dumpExp(op) }
+
+
+  override def visit(op: NotEqualExpression) = { dumpExp(op) }
+
+
+  override def visit(op: NotExpression) = { dumpExp(op) }
+
+
+  override def visit(op: IsNullExpression) = { dumpExp(op) }
+
+
+  override def visit(op: NegativeExpression) = { dumpExp(op) }
+
+
+  override def visit(op: AddExpression) = { dumpExp(op) }
+
+
+  override def visit(op: SubtractExpression) = { dumpExp(op) }
+
+
+  override def visit(op: MultiplyExpression) = { dumpExp(op) }
+
+
+  override def visit(op: ModExpression) = { dumpExp(op) }
+
+
+  override def visit(op: DivideExpression) = { dumpExp(op) }
+
+
+  override def visit(op: MapLookupExpression) = { dumpExp(op) }
+
+
+  override def visit(op: BinCondExpression) = { dumpExp(op) }
+
+
+  override def visit(op: UserFuncExpression) = { dumpExp(op) }
+
+
+  override def visit(op: DereferenceExpression) = { dumpExp(op) }
+
+
+  override def visit(op: RegexExpression) = { dumpExp(op) }
 }
--- sql/core/src/test/scala/org/apache/spark/sql/pig/PigLogicalPlanDumpVisitor.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/pig/PigLogicalPlanDumpVisitor.scala
@@ -1,8 +1,109 @@
 package org.apache.spark.sql.pig
 
-/**
- * Created by Greg on 6/19/14.
- */
-class PigLogicalPlanDumpVisitor {
+import org.apache.pig.newplan.{Operator, DependencyOrderWalker, OperatorPlan}
+import org.apache.pig.newplan.logical.relational._
+
+import scala.collection.JavaConversions._
+
+class PigLogicalPlanDumpVisitor(plan: OperatorPlan, indent: String = "")
+  extends LogicalRelationalNodesVisitor(plan, new DependencyOrderWalker(plan)) {
+
+  def printWithIndent(str: String) = {
+    println(indent + str)
+  }
+
+  def dumpOp(op: Operator) = {
+    printWithIndent(op.toString)
+
+    printWithIndent("   Predecessors:")
+    val preds = op.getPlan.getPredecessors(op)
+    if (preds == null) { printWithIndent("      null") }
+    else {
+      for (pred <- preds) {
+        printWithIndent("      " + pred)
+      }
+    }
+
+    printWithIndent("   Successors:")
+    val succs = op.getPlan.getSuccessors(op)
+    if (succs == null) { printWithIndent("      null") }
+    else {
+      for (succ <- succs) {
+        printWithIndent("      " + succ)
+      }
+    }
+    println()
+  }
+
+  override def visit(op: LOLoad) = { dumpOp(op) }
+
+
+  override def visit(op: LOFilter) = { dumpOp(op) }
+
+
+  override def visit(op: LOStore) = { dumpOp(op) }
+
+
+  override def visit(op: LOJoin) = { dumpOp(op) }
+
+
+  override def visit(op: LOForEach) = {
+    dumpOp(op)
+    printWithIndent("   Inner Plan:")
+    val child = new PigLogicalPlanDumpVisitor(op.getInnerPlan, indent + "      ")
+    child.visit()
+    println()
+  }
+
+
+  override def visit(op: LOGenerate) = {
+    dumpOp(op)
+    printWithIndent("   Output Plans:")
+    op.getOutputPlans.map({ p =>
+      val child = new PigLogicalExpressionPlanDumpVisitor(p, indent + "      ")
+      child.visit()
+    })
+    printWithIndent("   Plan:")
+    println(op.getPlan)
+  }
+
+
+  override def visit(op: LOInnerLoad) = { dumpOp(op) }
+
+
+  override def visit(op: LOCube) = { dumpOp(op) }
+
+
+  override def visit(op: LOCogroup) = { dumpOp(op) }
+
+
+  override def visit(op: LOSplit) = { dumpOp(op) }
+
+
+  override def visit(op: LOSplitOutput) = { dumpOp(op) }
+
+
+  override def visit(op: LOUnion) = { dumpOp(op) }
+
+
+  override def visit(op: LOSort) = { dumpOp(op) }
+
+
+  override def visit(op: LORank) = { dumpOp(op) }
+
+
+  override def visit(op: LODistinct) = { dumpOp(op) }
+
+
+  override def visit(op: LOLimit) = { dumpOp(op) }
+
+
+  override def visit(op: LOCross) = { dumpOp(op) }
+
+
+  override def visit(op: LOStream) = { dumpOp(op) }
+
+
+  override def visit(op: LONative) = { dumpOp(op) }
 
 }
