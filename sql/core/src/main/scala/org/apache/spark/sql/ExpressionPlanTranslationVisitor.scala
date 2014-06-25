67edd9637a4c7fe2bdf207da1c253533c2d85813
Gregory Owen <greowen@gmail.com>
2014-06-24 17:39:51 -0700

    Added support for more expressions

--- sql/core/src/main/scala/org/apache/spark/sql/ExpressionPlanTranslationVisitor.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/ExpressionPlanTranslationVisitor.scala
@@ -1,11 +1,12 @@
 package org.apache.spark.sql
 
 import org.apache.pig.newplan.logical.expression.{
-LogicalExpression => PigExpression, BinaryExpression => PigBinaryExpression, _}
+LogicalExpression => PigExpression, BinaryExpression => PigBinaryExpression, UnaryExpression => PigUnaryExpression _}
 import org.apache.pig.newplan.DependencyOrderWalker
 
 import org.apache.spark.sql.catalyst.expressions.{Expression => SparkExpression, _}
 import org.apache.spark.sql.catalyst.analysis.UnresolvedAttribute
+import org.apache.spark.sql.catalyst.types.StringType
 
 /**
  * Walks a Pig LogicalExpressionPlan tree and translates it into an equivalent Catalyst expression plan
@@ -13,6 +14,16 @@ import org.apache.spark.sql.catalyst.analysis.UnresolvedAttribute
 class ExpressionPlanTranslationVisitor(plan: LogicalExpressionPlan)
   extends LogicalExpressionVisitor(plan, new DependencyOrderWalker(plan, true))
   with PigTranslationVisitor[PigExpression, SparkExpression] {
+
+  // predicate ? left : right
+  override def visit(pigCond: BinCondExpression) {
+    val predicate = pigToSparkMap.get(pigCond.getCondition).getOrElse(throw new NoSuchElementException)
+    val left = pigToSparkMap.get(pigCond.getLhs).getOrElse(throw new NoSuchElementException)
+    val right = pigToSparkMap.get(pigCond.getRhs).getOrElse(throw new NoSuchElementException)
+
+    val cond = new If(predicate, left, right)
+    updateStructures(pigCond, cond)
+  }
 
   // TODO: Allow this to handle more general FuncSpec values (right now we can only handle casts to and from basic types)
   override def visit(pigCast: CastExpression) {
@@ -27,8 +38,26 @@ class ExpressionPlanTranslationVisitor(plan: LogicalExpressionPlan)
     val value = pigConst.getValue
     val sparkType = translateType(pigConst.getFieldSchema.`type`)
 
-    val constant = Literal(value, sparkType)
+    val constant = new Literal(value, sparkType)
     updateStructures(pigConst, constant)
+  }
+
+  override def visit(pigLookup: MapLookupExpression) {
+    val map = pigToSparkMap.get(pigLookup.getMap).getOrElse(throw new NoSuchElementException)
+    // Pig keys are always strings
+    val key = new Literal(pigLookup.getLookupKey, StringType)
+
+    val getItem = new GetItem(map, key)
+    updateStructures(pigLookup, getItem)
+  }
+
+  override def visit(pigNE: NotEqualExpression) {
+    val left = pigToSparkMap.get(pigNE.getLhs).getOrElse(throw new NoSuchElementException)
+    val right = pigToSparkMap.get(pigNE.getRhs).getOrElse(throw new NoSuchElementException)
+
+    val equals = new Equals(left, right)
+    val not = new Not(equals)
+    updateStructures(pigNE, not)
   }
 
   override def visit(pigProj: ProjectExpression) {
@@ -40,35 +69,57 @@ class ExpressionPlanTranslationVisitor(plan: LogicalExpressionPlan)
     updateStructures(pigProj, proj)
   }
 
+  // Unary Expressions
+  override def visit(pigExp: IsNullExpression)   { unaryExpression(pigExp) }
+  override def visit(pigExp: NegativeExpression) { unaryExpression(pigExp) }
+  override def visit(pigExp: NotExpression)      { unaryExpression(pigExp) }
+
+  protected def unaryExpression(pigExp: PigUnaryExpression) {
+    val exp = pigToSparkMap.get(pigExp.getExpression).getOrElse(throw new NoSuchElementException)
+
+    val sparkClass = pigExp match {
+      case _: IsNullExpression => IsNull
+      case _: NegativeExpression => UnaryMinus
+      case _: NotExpression => Not
+    }
+
+    val sparkExp = sparkClass(exp)
+    updateStructures(pigExp, sparkExp)
+  }
+
   // Binary Expressions
-  override def visit(pigExp: AndExpression)              { binaryExpression(pigExp) }
-  override def visit(pigExp: OrExpression)               { binaryExpression(pigExp) }
-  override def visit(pigExp: EqualExpression)            { binaryExpression(pigExp) }
-  override def visit(pigExp: LessThanExpression)         { binaryExpression(pigExp) }
-  override def visit(pigExp: LessThanEqualExpression)    { binaryExpression(pigExp) }
-  override def visit(pigExp: GreaterThanExpression)      { binaryExpression(pigExp) }
-  override def visit(pigExp: GreaterThanEqualExpression) { binaryExpression(pigExp) }
   override def visit(pigExp: AddExpression)              { binaryExpression(pigExp) }
-  override def visit(pigExp: SubtractExpression)         { binaryExpression(pigExp) }
-  override def visit(pigExp: MultiplyExpression)         { binaryExpression(pigExp) }
+  override def visit(pigExp: AndExpression)              { binaryExpression(pigExp) }
   override def visit(pigExp: DivideExpression)           { binaryExpression(pigExp) }
+  override def visit(pigExp: EqualExpression)            { binaryExpression(pigExp) }
+  override def visit(pigExp: GreaterThanEqualExpression) { binaryExpression(pigExp) }
+  override def visit(pigExp: GreaterThanExpression)      { binaryExpression(pigExp) }
+  override def visit(pigExp: LessThanEqualExpression)    { binaryExpression(pigExp) }
+  override def visit(pigExp: LessThanExpression)         { binaryExpression(pigExp) }
+  override def visit(pigExp: ModExpression)              { binaryExpression(pigExp) }
+  override def visit(pigExp: MultiplyExpression)         { binaryExpression(pigExp) }
+  override def visit(pigExp: OrExpression)               { binaryExpression(pigExp) }
+  override def visit(pigExp: RegexExpression)            { binaryExpression(pigExp) }
+  override def visit(pigExp: SubtractExpression)         { binaryExpression(pigExp) }
 
   protected def binaryExpression(pigExp: PigBinaryExpression) {
     val left = pigToSparkMap.get(pigExp.getLhs).getOrElse(throw new NoSuchElementException)
     val right = pigToSparkMap.get(pigExp.getRhs).getOrElse(throw new NoSuchElementException)
 
     val sparkClass = pigExp match {
-      case _: AndExpression => And
-      case _: OrExpression => Or
-      case _: EqualExpression => Equals
-      case _: LessThanExpression => LessThan
-      case _: LessThanEqualExpression => LessThanOrEqual
-      case _: GreaterThanExpression => GreaterThan
-      case _: GreaterThanEqualExpression => GreaterThanOrEqual
       case _: AddExpression => Add
-      case _: SubtractExpression => Subtract
-      case _: MultiplyExpression => Multiply
+      case _: AndExpression => And
       case _: DivideExpression => Divide
+      case _: EqualExpression => Equals
+      case _: GreaterThanEqualExpression => GreaterThanOrEqual
+      case _: GreaterThanExpression => GreaterThan
+      case _: LessThanEqualExpression => LessThanOrEqual
+      case _: LessThanExpression => LessThan
+      case _: ModExpression => Remainder
+      case _: MultiplyExpression => Multiply
+      case _: OrExpression => Or
+      case _: RegexExpression => Like
+      case _: SubtractExpression => Subtract
     }
 
     val sparkExp = sparkClass(left, right)
